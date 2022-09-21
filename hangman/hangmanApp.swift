695f4a0c5b58dfd9a6cf96a2e7c74f590b6c1fdb
VerDel <verdel@cargal.net>
2022-09-21 12:05:56 -0400

    Initial Commit

---
+++ hangman/hangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  hangmanApp.swift
+//  hangman
+//
+//  Created by VerDel Cargal on 9/21/22.
+//
+
+import SwiftUI
+
+@main
+struct hangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
