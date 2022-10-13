22681f30fb5faba34ae4e425a50efbe9efbb2071
VerDel <verdel@cargal.net>
2022-10-13 12:28:40 -0400

    mike refactoring of Gallows

--- hangman/Gallows.swift
+++ hangman/Gallows.swift
@@ -9,149 +9,165 @@ import SwiftUI
 
 struct Gallows: Shape {
     let guessesRemaining: Int
-    
-    let poleX = 0.8
-    let headRadiusPct = 0.06
-    let ropeX = 0.4
-    let lengthOfRope = 0.15
-    let torsoHeight = 0.20
-    let leglength = 0.30
-    let legAngle = deg2rad(70.0)
-    let armLength = 0.15
-    let armAngle = deg2rad(50.0)
-    let eyeXPercent = 0.3
-    let eyeYPercent = 0.8
-    let eyeRadiusPercent = 0.10
-    let mouthRadiusPercent = 0.2
-    
+     
     func path(in rect: CGRect) -> Path {
-        Path { path in
-            let bottomOfPole = CGPoint(x: rect.width * poleX, y: rect.maxY)
-            let topOfPole = CGPoint(x: bottomOfPole.x, y: rect.minY)
-            let endOfCrossBar = CGPoint(x: rect.width * ropeX, y: topOfPole.y)
-            let baseRight = CGPoint(x: rect.maxX, y: rect.maxY)
-            let baseLeft = CGPoint(x: rect.minX, y: baseRight.y)
+        var path = Path()
+        var guessesCutoff = HangmanGame.INCORRECT_GUESSES_ALLOWED
+        
+        let height = rect.height
+        let width = rect.width
+
+        let lengthOfRope = height * 0.15
+        let ropeX = width * 0.4
+         
+        let headRadius = height * 0.06
+        let torsoHeight = height * 0.20
+        
+        let endOfRope = CGPoint(x: ropeX, y: lengthOfRope)
+        let bottomOfHead = CGPoint(
+            x: endOfRope.x,
+            y: endOfRope.y + (headRadius * 2))
+        let bottomOfTorso = CGPoint(
+            x: bottomOfHead.x,
+            y: bottomOfHead.y + torsoHeight)
             
+        addGallows()
+        addHead()
+        addTorso()
+        addArms()
+        addLegs()
+        addEyes()
+        addMouth()
+        return path
+        
+        func addGallows() {
+            let poleX = width * 0.8
+            let bottomOfPole = CGPoint(x: poleX, y: height)
+            let topOfPole = CGPoint(x: bottomOfPole.x, y: rect.minY)
+            let endOfCrossBar = CGPoint(x: ropeX, y: topOfPole.y)
+
             path.move(to: bottomOfPole)
             path.addLine(to: topOfPole)
             path.addLine(to: endOfCrossBar)
-            path.addLine(to: endOfRope(rect))
-            
+            path.addLine(to: endOfRope)
+ 
+            let baseRight = CGPoint(x: width, y: height)
+            let baseLeft = CGPoint(x: rect.minX, y: baseRight.y)
+     
             path.move(to: baseLeft)
             path.addLine(to: baseRight)
+        }
+        
+        func addHead() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
             
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED { return }
-            addHead(&path, rect)
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED - 1 { return }
-            addTorso(&path, rect)
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED - 2 { return }
-            addArms(&path, rect)
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED - 3 { return }
-            addLegs(&path, rect)
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED - 4 { return }
-            addEyes(&path, rect)
-            if guessesRemaining == HangmanGame.INCORRECT_GUESSES_ALLOWED - 5 { return }
-            addMouth(&path, rect)
+            path.move(to: endOfRope)
+            path.addArc(
+                center: CGPoint(
+                    x: endOfRope.x,
+                    y: endOfRope.y + headRadius),
+                radius: headRadius,
+                startAngle: .degrees(270),
+                endAngle: .degrees(270 - 360),
+                clockwise: false
+            )
+        }
+        
+        func addTorso() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
+            
+            path.move(to: bottomOfHead)
+            path.addLine(to: bottomOfTorso)
+        }
+        
+        func addArms() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
+            
+            let armLength = height * 0.15
+            let armAngle = deg2rad(50)
+            
+            let shoulders = CGPoint(
+                x: bottomOfHead.x,
+                y: bottomOfHead.y + headRadius / 2)
+            let handXDelta = cos(armAngle) * armLength
+            let handY = shoulders.y + (sin(armAngle) * armLength)
+            let leftHandX = shoulders.x - handXDelta
+            let rightHandX = shoulders.x + handXDelta
+            
+            path.move(to: shoulders)
+            path.addLine(to: CGPoint(x: leftHandX, y: handY))
+            path.move(to: shoulders)
+            path.addLine(to: CGPoint(x: rightHandX, y: handY))
+        }
+        
+        func addLegs() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
+            
+            let legLength = height * 0.30
+            let legAngle = deg2rad(70)
+
+            let footXDelta = cos(legAngle) * legLength
+            let footY = bottomOfTorso.y + (sin(legAngle) * legLength)
+            let leftFootX = bottomOfTorso.x - footXDelta
+            let rightFootX = bottomOfTorso.x + footXDelta
+            
+            path.move(to: bottomOfTorso)
+            path.addLine(to: CGPoint(x: leftFootX, y: footY))
+            path.move(to: bottomOfTorso)
+            path.addLine(to: CGPoint(x: rightFootX, y: footY))
+        }
+        
+        func addEyes() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
+  
+            let eyeXDelta = headRadius * 0.3
+            let eyeY = endOfRope.y + headRadius * 0.8
+            let eyeRadius = headRadius * 0.1
+     
+            let leftEyeX = endOfRope.x - eyeXDelta
+            let rightEyeX = endOfRope.x + eyeXDelta
+            
+            path.move(to: CGPoint(x: rightEyeX, y: eyeY - eyeRadius))
+            path.addArc(
+                center: CGPoint(x: rightEyeX, y: eyeY),
+                radius: eyeRadius,
+                startAngle: .degrees(270),
+                endAngle: .degrees(270 - 360),
+                clockwise: false
+            )
+            path.move(to: CGPoint(x: leftEyeX, y: eyeY - eyeRadius))
+            path.addArc(
+                center: CGPoint(x: leftEyeX, y: eyeY),
+                radius: eyeRadius,
+                startAngle: .degrees(270),
+                endAngle: .degrees(270 - 360),
+                clockwise: false
+            )
+        }
+        
+        func addMouth() {
+            if guessesRemaining >= guessesCutoff { return }
+            guessesCutoff -= 1
+            
+            let mouthRadius = headRadius * 0.2
+      
+            let mouthCenter = CGPoint(x: bottomOfHead.x, y: bottomOfHead.y - mouthRadius * 2)
+            
+            path.move(to: CGPoint(x: bottomOfHead.x + mouthRadius, y: mouthCenter.y))
+            path.addArc(center: mouthCenter,
+                        radius: mouthRadius,
+                        startAngle: .degrees(0),
+                        endAngle: .degrees(180),
+                        clockwise: true)
         }
     }
-    
-    private func addHead(_ path: inout Path, _ rect: CGRect) {
-        let radius = rect.height * headRadiusPct
-        let endOfRope = endOfRope(rect)
-        
-        path.move(to: endOfRope)
-        path.addArc(
-            center: CGPoint(x: endOfRope.x, y: endOfRope.y + radius),
-            radius: radius,
-            startAngle: .degrees(270),
-            endAngle: .degrees(270 - 360),
-            clockwise: false
-        )
-    }
-    
-    private func addTorso(_ path: inout Path, _ rect: CGRect) {
-        path.move(to: bottomOfHead(rect))
-        path.addLine(to: bottomOfTorso(rect))
-    }
-    
-    private func addArms(_ path: inout Path, _ rect: CGRect) {
-        let bottomOfHead = bottomOfHead(rect)
-        let shoulders = CGPoint(x: bottomOfHead.x, y: bottomOfHead.y + rect.height * headRadiusPct / 2)
 
-        let handXDelta = cos(armAngle) * (rect.height * armLength)
-        let handY = shoulders.y + sin(armAngle) * rect.height * armLength
-        
-        path.move(to: shoulders)
-        path.addLine(to: CGPoint(x: shoulders.x - handXDelta, y: handY))
-        path.move(to: shoulders)
-        path.addLine(to: CGPoint(x: shoulders.x + handXDelta, y: handY))
-    }
-    
-    private func addLegs(_ path: inout Path, _ rect: CGRect) {
-        let bottomOfTorso = bottomOfTorso(rect)
-        let footXDelta = cos(legAngle) * (rect.height * leglength)
-        let footY = bottomOfTorso.y + sin(legAngle) * rect.height * leglength
-        
-        path.move(to: bottomOfTorso)
-        path.addLine(to: CGPoint(x: bottomOfTorso.x - footXDelta, y: footY))
-        path.move(to: bottomOfTorso)
-        path.addLine(to: CGPoint(x: bottomOfTorso.x + footXDelta, y: footY))
-    }
-    
-    private func addEyes(_ path: inout Path, _ rect: CGRect) {
-        let endOfRope = endOfRope(rect)
-        let headRadius = rect.height * headRadiusPct
-        let eyeXDelta = headRadius * eyeXPercent
-        let eyeY = endOfRope.y + headRadius * eyeYPercent
-        let leftEyeX = endOfRope.x - eyeXDelta
-        let rightEyeX = endOfRope.x + eyeXDelta
-        
-        path.move(to: CGPoint(x: rightEyeX, y: eyeY - headRadius * eyeRadiusPercent))
-        path.addArc(
-            center: CGPoint(x: rightEyeX, y: eyeY),
-            radius: headRadius * eyeRadiusPercent,
-            startAngle: .degrees(270),
-            endAngle: .degrees(270 - 360),
-            clockwise: false
-        )
-        path.move(to: CGPoint(x: leftEyeX, y: eyeY - headRadius * eyeRadiusPercent))
-        path.addArc(
-            center: CGPoint(x: leftEyeX, y: eyeY),
-            radius: headRadius * eyeRadiusPercent,
-            startAngle: .degrees(270),
-            endAngle: .degrees(270 - 360),
-            clockwise: false
-        )
-    }
-    
-    private func addMouth(_ path: inout Path, _ rect: CGRect) {
-        let headRadius = rect.height * headRadiusPct
-        let bottomOfHead = bottomOfHead(rect)
-        let mouthRadius =  headRadius * mouthRadiusPercent
-        let mouthCenter = CGPoint(x: bottomOfHead.x, y: bottomOfHead.y - mouthRadius * 2)
-        
-        path.move(to: CGPoint(x: bottomOfHead.x + mouthRadius, y: mouthCenter.y))
-        path.addArc(center: mouthCenter,
-                    radius: mouthRadius,
-                    startAngle: .degrees(0),
-                    endAngle: .degrees(180),
-                    clockwise: true)
-    }
-    
-    private func endOfRope(_ rect: CGRect) -> CGPoint {
-        CGPoint(x: rect.width * ropeX, y: rect.height * lengthOfRope)
-    }
-    
-    private func bottomOfHead(_ rect: CGRect) -> CGPoint {
-        CGPoint(x: endOfRope(rect).x,
-                y: endOfRope(rect).y + (rect.height * headRadiusPct * 2))
-    }
-
-    private func bottomOfTorso(_ rect: CGRect) -> CGPoint {
-        CGPoint(x: bottomOfHead(rect).x, y: bottomOfHead(rect).y + rect.height * torsoHeight)
-    }
-
-    static func deg2rad(_ number: Double) -> Double {
+    func deg2rad(_ number: Double) -> Double {
         return number * .pi / 180
     }
 }
@@ -166,7 +182,7 @@ struct Gallows_Preview: PreviewProvider {
                 .background(.yellow)
             Spacer()
             Gallows(guessesRemaining: 0)
-                .stroke(.red, lineWidth: 3)
+                .stroke(.red, lineWidth: 1)
                 .frame(width: 70.0, height: 130.0)
                 .background(.green)
             Spacer()
